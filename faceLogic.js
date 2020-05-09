951375feea63855f7c8aa023e6778df1aa802e15
drabon2020 <65034202+drabon2020@users.noreply.github.com>
2020-05-08 22:31:52 -0400

    Update faceLogic.js

--- faceLogic.js
+++ faceLogic.js
@@ -50,7 +50,7 @@ function StartAR(image) {
 
             const resizedDetections = faceapi.resizeResults(detections, displaySize)
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-            faceapi.draw.drawDetections(canvas, resizedDetections)
+            //faceapi.draw.drawDetections(canvas, resizedDetections)
             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
             //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
             // draw detections into the canvas
