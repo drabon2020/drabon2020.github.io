1aac5a606561f99d133c40f0eb4b9488b05a0e8d
drabon2020 <65034202+drabon2020@users.noreply.github.com>
2020-05-11 09:06:14 -0400

    Update faceLogic.js

--- faceLogic.js
+++ faceLogic.js
@@ -44,7 +44,7 @@ function StartAR(image) {
                 found = false;
             }
             else if (detections.length > 0 && !found) {
-                ruisantos.FaceComponent5.StaticInstance.ReceiveData("Found a Face");
+                ruisantos.FaceComponent5.StaticInstance.ReceiveData("Math.round(detection.age) + " year old " + detection.gender");
                 found = true;
             }
             ruisantos.FaceComponent5.StaticInstance.refreshData();
